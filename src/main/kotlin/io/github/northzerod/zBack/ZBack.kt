88f7928b2e5a0732123e71c5693d09ab4cf7d84b
NorthZeroD <northzero_9376@outlook.com>
2025-12-06 18:37:00 +0800

    清理无用成员函数; 增加几个静态字段存储单例对象

--- src/main/kotlin/io/github/northzerod/zBack/ZBack.kt
+++ src/main/kotlin/io/github/northzerod/zBack/ZBack.kt
@@ -2,36 +2,33 @@ package io.github.northzerod.zBack
 
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitScheduler
 import java.util.logging.Logger
 
 
 class ZBack : JavaPlugin() {
     companion object {
         lateinit var plg: ZBack
+        lateinit var log: Logger
         lateinit var dbm: DatabaseManager
+        lateinit var sch: BukkitScheduler
     }
 
     override fun onEnable() {
-        logger.info("Hello!")
         plg = this
+        log = plg.logger
         saveDefaultConfig()
         dbm = DatabaseManager("jdbc:sqlite:plugins/ZBack/database.db")
+        sch = this.server.scheduler
         server.pluginManager.registerEvents(PlayerDeathListener(), this)
         lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
-            commands.registrar().register(BackCommand().get())
+            commands.registrar().register(BackCommand.backCommand)
+            commands.registrar().register(BackCommand.bCommand)
         }
+        log.info("Enabled ZBack :D")
     }
 
     override fun onDisable() {
         dbm.close()
     }
-
-    override fun getLogger(): Logger {
-        return super.getLogger()
-    }
-
-    override fun saveDefaultConfig() {
-        super.saveDefaultConfig()
-    }
-
 }
